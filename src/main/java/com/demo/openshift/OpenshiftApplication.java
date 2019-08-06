cf683b8368d362b7babd27d0cedf12d26d91eaf9
hansj.melby <hansj.melby@gmail.com>
2019-08-06 15:01:11 +0200

    initial commit

---
+++ src/main/java/com/demo/openshift/OpenshiftApplication.java
@@ -0,0 +1,35 @@
+package com.demo.openshift;
+
+import com.demo.openshift.dto.Greeting;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+//@SpringBootApplication(scanBasePackages = {"com.demo.openshift"})
+@SpringBootApplication
+//@RestController
+public class OpenshiftApplication {
+
+/*
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+  */
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftApplication.class, args);
+    }
+
+}
